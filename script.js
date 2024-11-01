3da848c7122182cdbd92565ec611d7dc33d88baa
Melis Yetis <e263215@sbl.ch>
2024-11-01 13:45:24 +0000

    Aufgabe 9 fertig

--- script.js
+++ script.js
@@ -110,4 +110,23 @@ export function aufgabe08(args) {
 }
 
 linkupExerciseHandler("[data-click=aufgabe08]", aufgabe08)
-export function aufgabe09(args) {}
+
+export function aufgabe09(args) {
+  const input = args
+  const result = []
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    count = count + 1
+  }
+  let has6 = false
+
+  if (count === 6) {
+    has6 = true
+  }
+
+  return has6
+}
+linkupExerciseHandler("[data-click=aufgabe09]", aufgabe09)
+
+export function aufgabe11(args) {}
